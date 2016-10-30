7af6c3ff2aec36d4ba920df9ebd9eeffed30a39a
quangthinh <quangthinh.nguyen91@gmail.com>
2016-10-30 22:46:08 +0700

    update post

---
+++ Assets/MyData.cs
@@ -0,0 +1,48 @@
+﻿/// <summary>
+/// Luu du lieu nguoi dung theo session
+/// </summary>
+public class MySession
+{
+    //{
+    // ses_id: 0,
+    // use_id: 0,
+    // ses_time_start: "",
+    // ses_time_end: "",
+    // ses_score: 0,
+    // ses_status: ""
+    //}
+    public int ses_id { get; set; }
+    public int use_id { get; set; }
+    public string ses_time_start { get; set; }
+    public string ses_time_end { get; set; }
+    public int ses_score { get; set; }
+    public string ses_status { get; set; }
+}
+
+
+public class Rootobject
+{
+    //{
+	   // use_id: 0,
+	   // use_first_name: "",
+	   // use_last_name: "",
+	   // use_dob: "",
+	   // use_score: 0,
+	   // use_level: 0,
+	   // use_count_play: 0,
+	   // created_at: "",
+	   // use_status: ""
+    //}
+
+    public int use_id { get; set; }
+    public string use_first_name { get; set; }
+    public string use_last_name { get; set; }
+    public string use_dob { get; set; }
+    public int use_score { get; set; }
+    public int use_level { get; set; }
+    public int use_count_play { get; set; }
+    public string created_at { get; set; }
+    public string use_status { get; set; }
+}
+
+
---
+++ Assets/Scripts/CreateDBScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System;
+
+public class CreateDBScript : MonoBehaviour {
+
+	public Text DebugText;
+
+	// Use this for initialization
+	void Start () {
+		StartSync();
+	}
+
+    private void StartSync()
+    {
+        //Create new database with MySession name
+        var ds = new DataService("MySession.db");
+        //Xóa table Mysession
+        ds._connection.DropTable<MySession>();
+        //Tạo mới table Mysession
+        ds._connection.CreateTable<MySession>();
+
+        //Thêm thông tin vào bản
+        MySession mySession = new MySession();
+        mySession.ses_id = 1;
+        mySession.use_id = 1;
+        mySession.ses_score = 1;
+        mySession.ses_time_start = DateTime.Now.ToString("y-M-d H:m:s"); ;
+        mySession.ses_status = "enable";
+        ds._connection.Insert(mySession);
+
+        //Đọc dữ liệu sau khi insert vào table
+        var querySessionData = ds._connection.Query<MySession>("SELECT * FROM MySession");
+        Debug.Log(querySessionData.Count +"|"+ querySessionData[0].ses_time_start);
+                                                                                    
+
+    }
+	
+	private void ToConsole(IEnumerable<Person> people){
+		foreach (var person in people) {
+			ToConsole(person.ToString());
+		}
+	}
+	
+	private void ToConsole(string msg){
+		DebugText.text += System.Environment.NewLine + msg;
+		Debug.Log (msg);
+	}
+}
